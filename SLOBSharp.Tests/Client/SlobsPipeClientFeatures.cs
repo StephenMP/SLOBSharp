e59a7a17ef3ac4053ac4942d7f9cb53adde69976
StephenMP <1.stephen.porter@gmail.com>
2018-10-30 11:40:44 -0600

    More tests added

---
+++ SLOBSharp.Tests/Client/SlobsPipeClientFeatures.cs
@@ -0,0 +1,103 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+namespace SLOBSharp.Tests.Client
+{
+    public class SlobsPipeClientFeatures
+    {
+        private readonly SlobsPipeClientSteps steps;
+
+        public SlobsPipeClientFeatures()
+        {
+            this.steps = new SlobsPipeClientSteps();
+        }
+
+        [Fact]
+        public void CanCreateWithoutAPipeName()
+        {
+            this.steps.WhenINewUpAPipeClient();
+
+            this.steps.ThenIShouldHaveCreatedANewPipeClient();
+        }
+
+        [Fact]
+        public void CanCreateWithAPipeName()
+        {
+            this.steps.WhenINewUpAPipeClient("slobs");
+
+            this.steps.ThenIShouldHaveCreatedANewPipeClient();
+        }
+
+        [Fact]
+        public void CanCreateWithoutAPipeNameUsingStaticCreator()
+        {
+            this.steps.WhenINewUpAPipeClientUsingStaticCreator();
+
+            this.steps.ThenIShouldHaveCreatedANewPipeClient();
+        }
+
+        [Fact]
+        public void CanCreateWithAPipeNameUsingStaticCreator()
+        {
+            this.steps.WhenINewUpAPipeClientUsingStaticCreator("slobs");
+
+            this.steps.ThenIShouldHaveCreatedANewPipeClient();
+        }
+
+        [Fact]
+        public void CanExecuteARequest()
+        {
+            this.steps.GivenIHaveARequest();
+            this.steps.GivenIHaveAMockedResponse();
+            this.steps.GivenIHaveAMockedSlobsPipeService();
+            this.steps.GivenIHaveASlobsPipeClient();
+
+            this.steps.WhenICallExecuteRequest();
+
+            this.steps.ThenIShouldReceiveAResponse();
+            this.steps.ThenTheResponseShouldBeTheMockedResponse();
+        }
+
+        [Fact]
+        public void CanExecuteMultipleRequests()
+        {
+            this.steps.GivenIHaveMultipleRequests(10);
+            this.steps.GivenIHaveMultipleMockedResponse();
+            this.steps.GivenIHaveAMockedSlobsPipeService();
+            this.steps.GivenIHaveASlobsPipeClient();
+
+            this.steps.WhenICallExecuteRequests();
+
+            this.steps.ThenIShouldReceiveResponses();
+            this.steps.ThenTheResponsesShouldBeTheMockedResponses();
+        }
+
+        [Fact]
+        public async Task CanExecuteMultipleRequestsAsync()
+        {
+            this.steps.GivenIHaveMultipleRequests(10);
+            this.steps.GivenIHaveMultipleMockedResponse();
+            this.steps.GivenIHaveAMockedSlobsPipeService();
+            this.steps.GivenIHaveASlobsPipeClient();
+
+            await this.steps.WhenICallExecuteRequestsAsync().ConfigureAwait(false);
+
+            this.steps.ThenIShouldReceiveResponses();
+            this.steps.ThenTheResponsesShouldBeTheMockedResponses();
+        }
+
+        [Fact]
+        public async Task CanExecateARequestAsync()
+        {
+            this.steps.GivenIHaveARequest();
+            this.steps.GivenIHaveAMockedResponse();
+            this.steps.GivenIHaveAMockedSlobsPipeService();
+            this.steps.GivenIHaveASlobsPipeClient();
+
+            await this.steps.WhenICallExecuteRequestAsync().ConfigureAwait(false);
+
+            this.steps.ThenIShouldReceiveAResponse();
+            this.steps.ThenTheResponseShouldBeTheMockedResponse();
+        }
+    }
+}
--- SLOBSharp/Client/SlobsPipeClient.cs
+++ SLOBSharp/Client/SlobsPipeClient.cs
@@ -1,158 +1,7 @@
-﻿using System.Collections.Generic;
-using System.Threading.Tasks;
-using SLOBSharp.Client.Requests;
-using SLOBSharp.Client.Responses;
-using SLOBSharp.Domain.Services;
+﻿using SLOBSharp.Domain.Services;
 
 namespace SLOBSharp.Client
 {
-    public enum SlobsClientType
-    {
-        Pipe,
-        WebSocket
-    }
-
-    public interface ISlobsClient
-    {
-        /// <summary>
-        /// Executes a single request synchronously.
-        /// </summary>
-        /// <param name="request">The request.</param>
-        /// <returns></returns>
-        SlobsRpcResponse ExecuteRequest(ISlobsRequest request);
-
-        /// <summary>
-        /// Executes a single request asynchronously.
-        /// </summary>
-        /// <param name="request">The request.</param>
-        /// <returns></returns>
-        Task<SlobsRpcResponse> ExecuteRequestAsync(ISlobsRequest request);
-
-        /// <summary>
-        /// Executes multiple requests synchronously.
-        /// </summary>
-        /// <param name="requests">The requests.</param>
-        /// <returns></returns>
-        IEnumerable<SlobsRpcResponse> ExecuteRequests(params ISlobsRequest[] requests);
-
-        /// <summary>
-        /// Executes multiple requests synchronously.
-        /// </summary>
-        /// <param name="requests">The requests.</param>
-        /// <returns></returns>
-        IEnumerable<SlobsRpcResponse> ExecuteRequests(IEnumerable<ISlobsRequest> requests);
-
-        /// <summary>
-        /// Executes multiple requests asynchronously.
-        /// </summary>
-        /// <param name="requests">The requests.</param>
-        /// <returns></returns>
-        Task<IEnumerable<SlobsRpcResponse>> ExecuteRequestsAsync(params ISlobsRequest[] requests);
-
-        /// <summary>
-        /// Executes multiple requests asynchronously.
-        /// </summary>
-        /// <param name="requests">The requests.</param>
-        /// <returns></returns>
-        Task<IEnumerable<SlobsRpcResponse>> ExecuteRequestsAsync(IEnumerable<ISlobsRequest> requests);
-    }
-
-    public interface ISlobsConnectionInfo
-    {
-        string PipeName { get; }
-        SlobsClientType SlobsClientType { get; }
-    }
-
-    public abstract class SlobsClient : ISlobsClient
-    {
-        private readonly ISlobsService slobsService;
-
-        public ISlobsConnectionInfo SlobsConnectionInfo { get; protected set; }
-
-        internal SlobsClient(ISlobsService slobsService)
-        {
-            this.slobsService = slobsService;
-        }
-
-        /// <summary>
-        /// Executes a single request synchronously.
-        /// </summary>
-        /// <param name="request">The request.</param>
-        /// <returns></returns>
-        public SlobsRpcResponse ExecuteRequest(ISlobsRequest request)
-        {
-            return this.slobsService.ExecuteRequest(request);
-        }
-
-        /// <summary>
-        /// Executes a single request asynchronously.
-        /// </summary>
-        /// <param name="request">The request.</param>
-        /// <returns></returns>
-        public async Task<SlobsRpcResponse> ExecuteRequestAsync(ISlobsRequest request)
-        {
-            return await this.slobsService.ExecuteRequestAsync(request).ConfigureAwait(false);
-        }
-
-        /// <summary>
-        /// Executes multiple requests synchronously.
-        /// </summary>
-        /// <param name="requests">The requests.</param>
-        /// <returns></returns>
-        public IEnumerable<SlobsRpcResponse> ExecuteRequests(params ISlobsRequest[] requests)
-        {
-            return this.slobsService.ExecuteRequests(requests);
-        }
-
-        /// <summary>
-        /// Executes multiple requests synchronously.
-        /// </summary>
-        /// <param name="requests">The requests.</param>
-        /// <returns></returns>
-        public IEnumerable<SlobsRpcResponse> ExecuteRequests(IEnumerable<ISlobsRequest> requests)
-        {
-            return this.slobsService.ExecuteRequests(requests);
-        }
-
-        /// <summary>
-        /// Executes multiple requests asynchronously.
-        /// </summary>
-        /// <param name="requests">The requests.</param>
-        /// <returns></returns>
-        public async Task<IEnumerable<SlobsRpcResponse>> ExecuteRequestsAsync(params ISlobsRequest[] requests)
-        {
-            return await this.slobsService.ExecuteRequestsAsync(requests).ConfigureAwait(false);
-        }
-
-        /// <summary>
-        /// Executes multiple requests asynchronously.
-        /// </summary>
-        /// <param name="requests">The requests.</param>
-        /// <returns></returns>
-        public async Task<IEnumerable<SlobsRpcResponse>> ExecuteRequestsAsync(IEnumerable<ISlobsRequest> requests)
-        {
-            return await this.slobsService.ExecuteRequestsAsync(requests).ConfigureAwait(false);
-        }
-    }
-
-    public class SlobsConnectionInfo : ISlobsConnectionInfo
-    {
-        public string PipeName { get; }
-
-        public SlobsClientType SlobsClientType { get; }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="SlobsConnectionInfo"/> class.
-        /// </summary>
-        /// <param name="slobsClientType">Type of SLOBS client.</param>
-        /// <param name="pipeName">Name of the pipe.</param>
-        public SlobsConnectionInfo(SlobsClientType slobsClientType, string pipeName)
-        {
-            this.SlobsClientType = slobsClientType;
-            this.PipeName = pipeName;
-        }
-    }
-
     public class SlobsPipeClient : SlobsClient
     {
         /// <summary>
@@ -166,9 +15,12 @@ namespace SLOBSharp.Client
         /// Initializes a new instance of the <see cref="SlobsPipeClient"/> class.
         /// </summary>
         /// <param name="pipeName">Name of the pipe.</param>
-        public SlobsPipeClient(string pipeName) : base(new SlobsPipeService(pipeName))
+        public SlobsPipeClient(string pipeName) : this(new SlobsPipeService(pipeName))
         {
-            this.SlobsConnectionInfo = new SlobsConnectionInfo(SlobsClientType.Pipe, pipeName);
+        }
+
+        internal SlobsPipeClient(ISlobsService slobsService) : base(slobsService)
+        {
         }
     }
 }
